5a36df54533b3ee53c3220f8be0df52ca0982c02
FelixWaititu <123945606+FelixWaititu@users.noreply.github.com>
2024-06-10 07:46:21 +0300

    MINOR CHANGES

--- config/passport-google.js
+++ config/passport-google.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 passport.use( new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/redirect'
+      callbackURL: 'https://xtreamleague.onrender.com/auth/google/redirect'
 
     }, (accessToken, refreshToken, profile, done) => {
        //Check if User exists in the site
