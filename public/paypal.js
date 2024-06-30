ee9e178297d4f70d40e80cf14e4c43e5e2b124a1
FelixWaititu <123945606+FelixWaititu@users.noreply.github.com>
2024-06-30 16:37:35 +0300

    span forgotten

--- public/paypal.js
+++ public/paypal.js
@@ -7,6 +7,7 @@ const mpesaContainer = document.querySelector('.mpesa-wrapper')
 const lipaBtn = document.getElementById('lipa-mpesa')
 const mpesaCloseBtn = document.querySelector('.close-mpesa')
 const mpesaAmount = document.getElementById('mpesa-amount')
+const displaySpan = document.getElementById('displayAmount') 
 
 
 
@@ -45,29 +46,36 @@ function purchase(token) {
     if (token == 100 ) {
         inputAmount.setAttribute('value', 1.05)
         mpesaAmount.setAttribute('value', 100)
+        displaySpan.textContent = 100
         display()
     } else if (token == 200 ) {
         inputAmount.setAttribute('value', 1.75)
         mpesaAmount.setAttribute('value', 200)
+        displaySpan.textContent = 200
         display()
     } else if (token == 500 ) {
         inputAmount.setAttribute('value', 4.25)
         mpesaAmount.setAttribute('value', 500)
+        displaySpan.textContent = 500
         display()
     } else if (token == 1000 ) {
         inputAmount.setAttribute('value', 8.15)
         mpesaAmount.setAttribute('value', 1000)
+        displaySpan.textContent = 1000
         display()
     } else if (token == 2000 ) {
         inputAmount.setAttribute('value', 16.45)
         mpesaAmount.setAttribute('value', 2000)
+        displaySpan.textContent = 2000
         display()
     } else if (token == 5000 ) {
         inputAmount.setAttribute('value', 39.25)
         mpesaAmount.setAttribute('value', 5000)
+        displaySpan.textContent = 5000
         display()
     }
-   
+    
+
 }
 
 
