0e7ca3d84075986b5b1c11cac8163587514fae8e
unknown <alvinkarms5511@gmail.com>
2024-07-12 21:11:04 +0300

    stage DevWithdraw

--- models/xtreamUsers.js
+++ models/xtreamUsers.js
@@ -3,20 +3,20 @@ const Schema = mongoose.Schema
 
 
 const xtreamSchema = new Schema({
-     
-        userName: { type: String, required: true },
-        googleId: { type: String, required: true },
-          teamId: { type: String, default: 'none', required: false },
-     managerName: { type: String, default: 'none', required: false },
-        teamName: { type: String, default: 'none', required: false },
-    totalBalance: { type: Number, default: 0.00, required: false  },
-          payPal: { type: String, default: 'none', required: false },
-           mpesa: { type: String, default: 254, required: false},
-         favTeam: { type: Number, default: 0, required: false },
-          points: { type: Array, default: [], required: false },
-     totalEarned: { type: Number, default: 0, required: false }    
 
-}, {timestamps: true})
+     userName: { type: String, required: true },
+     googleId: { type: String, required: true },
+     teamId: { type: String, default: 'none', required: false },
+     managerName: { type: String, default: 'none', required: false },
+     teamName: { type: String, default: 'none', required: false },
+     totalBalance: { type: Number, default: 0.00, required: false },
+     payPal: { type: String, default: 'none', required: false },
+     mpesa: { type: Number, default: 254, required: false },
+     favTeam: { type: Number, default: 0, required: false },
+     points: { type: Array, default: [], required: false },
+     totalEarned: { type: Number, default: 0, required: false }
+
+}, { timestamps: true })
 
 const User = new mongoose.model('xtreamUsers', xtreamSchema)
 
--- routes/mpesa.js
+++ routes/mpesa.js
@@ -7,16 +7,16 @@ const Transactions = require('../models/transactions')
 const User = require('../models/xtreamUsers')
 
 
-const getAccessToken =  async () => {
+const getAccessToken = async () => {
     const accessUrl = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-    const consumerKey = process.env.CONSUMER_KEY 
+    const consumerKey = process.env.CONSUMER_KEY
     const secret = process.env.CONSUMER_SECRET
     const auth = new Buffer.from(`${consumerKey}:${secret}`).toString('base64')
     const Headers = {
-          headers: {
+        headers: {
             'Authorization': `Basic ${auth}`
-                  }
-          }
+        }
+    }
 
     try {
 
@@ -24,173 +24,173 @@ const getAccessToken =  async () => {
         const token = response.data.access_token
 
         return token
-        
+
     } catch (error) {
 
-        
+
         const token = null
         return token
     }
-     
-     
+
+
 }
 
 
 router.post('/stk', async (req, res) => {
-     
+
     const { mpesaAmount, phone } = req.body
-    
+
     const requiredlength = phone.toString().length
     const startingNum = phone.toString().charAt(0)
 
-    if ( requiredlength !== 10 && startingNum !== 0 ) {
+    if (requiredlength !== 10 && startingNum !== 0) {
 
-         req.flash('error', 'Incorrect Mpesa Number')
-         res.redirect('/deposit')
-    } else {
-        
-        // TODO: MPESA API LOGIC
-    const token = await getAccessToken()
-
-    if (token == null) {
-    
-        req.flash('error', 'AccessToken Error')
+        req.flash('error', 'Incorrect Mpesa Number')
         res.redirect('/deposit')
-
     } else {
 
-    const inputPhone = phone.substring(1)
-    const correctPhone = `254${inputPhone}`
-    const stkUrl = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
-    const passkey = process.env.PASSKEY
-    const shortcode = process.env.SHORTCODE 
-    const date = new Date()
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1 < 10 ?  `0${date.getMonth() + 1}` : date.getMonth() + 1
-    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
-    const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-    const seconds = date.getSeconds() < 10 ?  `0${date.getSeconds()}` : date.getSeconds()
-    const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`
+        // TODO: MPESA API LOGIC
+        const token = await getAccessToken()
 
-    const password = new Buffer.from(shortcode + passkey + timestamp).toString('base64')
+        if (token == null) {
+
+            req.flash('error', 'AccessToken Error')
+            res.redirect('/deposit')
+
+        } else {
+
+            const inputPhone = phone.substring(1)
+            const correctPhone = `254${inputPhone}`
+            const stkUrl = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+            const passkey = process.env.PASSKEY
+            const shortcode = process.env.SHORTCODE
+            const date = new Date()
+            const year = date.getFullYear()
+            const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+            const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+            const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+            const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+            const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+            const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`
+
+            const password = new Buffer.from(shortcode + passkey + timestamp).toString('base64')
 
 
 
-    await axios.post(stkUrl,  
-         {
-            "BusinessShortCode": shortcode,    
-            "Password": password,    
-            "Timestamp": timestamp,    
-            "TransactionType": "CustomerBuyGoodsOnline",    
-            "Amount": mpesaAmount,    
-            "PartyA": correctPhone,    
-            "PartyB": '8811882',    
-            "PhoneNumber": correctPhone,    
-            "CallBackURL": `https://xtreamleague.onrender.com/mpesa/${process.env.CALLBACK}`,    
-            "AccountReference": correctPhone,    
-            "TransactionDesc": "Test" 
-         },
-         {
-         headers: {
-            Authorization: `Bearer ${token}`
-         }
+            await axios.post(stkUrl,
+                {
+                    "BusinessShortCode": shortcode,
+                    "Password": password,
+                    "Timestamp": timestamp,
+                    "TransactionType": "CustomerBuyGoodsOnline",
+                    "Amount": mpesaAmount,
+                    "PartyA": correctPhone,
+                    "PartyB": '8811882',
+                    "PhoneNumber": correctPhone,
+                    "CallBackURL": `https://xtreamleague.onrender.com/mpesa/${process.env.CALLBACK}`,
+                    "AccountReference": correctPhone,
+                    "TransactionDesc": "Test"
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+
+            )
+                .then((response) => {
+
+
+                    const merchantID = response.data.MerchantRequestID
+
+                    res.redirect(`/mpesa?reqID=${merchantID}`)
+                })
+                .catch((err) => {
+
+                    req.flash('error', 'STK PUSH ERROR')
+                    res.redirect('/deposit')
+                })
+
+
         }
-        
-    )
-            .then((response) => {
-                
-                
-                const merchantID = response.data.MerchantRequestID
-
-                res.redirect(`/mpesa?reqID=${merchantID}`)
-            })
-            .catch((err) => {
-            
-                req.flash('error', 'STK PUSH ERROR')
-                res.redirect('/deposit')
-            })
-
-
     }
-  }
 })
 
 
 router.post(`/${process.env.CALLBACK}`, (req, res) => {
     const mpesaResponse = req.body
-    
-    
+
+
     const stkCallback = mpesaResponse.Body.stkCallback
     const metaData = mpesaResponse.Body.stkCallback.CallbackMetadata
 
-   if (!metaData) {
-      
-      const merchantID = stkCallback.MerchantRequestID
-      const resCode = stkCallback.ResultCode
-      const resDesc = stkCallback.ResultDesc
-      
-    //   TODO: SAVE FAILED TRANSACTION
+    if (!metaData) {
 
-      const tranx = new Transactions({
-         
-           tranx_type: 'Deposit',
-               method: 'mpesa',
-               userId: 'failed',
-               status: 'complete',
-          mpesaObject: {
-                   reqID: merchantID,
-                   status: {
-                       resultCode: resCode,
-                       resultDesc: resDesc 
-           }  
-      } 
-          
-      }).save().then((data) => {
-        
-        console.log('--INCOMPLETE TRANX SAVED--')
-      }).catch((err) => {
-        console.log('--FAILED TO SAVE TRANX--')
-      })
+        const merchantID = stkCallback.MerchantRequestID
+        const resCode = stkCallback.ResultCode
+        const resDesc = stkCallback.ResultDesc
 
-      return res.json({ message: 'Response OK'})
+        //   TODO: SAVE FAILED TRANSACTION
 
-   } else {
+        const tranx = new Transactions({
 
-      const merchantID = stkCallback.MerchantRequestID
-      const mpesaAmount = metaData.Item[0].Value
-      const phoneNumber = metaData.Item[4].Value
-      const succResCode = stkCallback.ResultCode
-      const succResDesc = stkCallback.ResultDesc
-      
-    // todo: SAVE SUCCESSFUL TRANSACTION
-    
-      const newSuccTranx = new Transactions({
             tranx_type: 'Deposit',
-                method: 'mpesa',
-                userId: 'success',
-                amount: mpesaAmount,
-                status: 'pending',
-           mpesaObject: {
-            mpesaNumber: phoneNumber,
-                  reqID: merchantID,
-                 status: {
-                      resultCode: succResCode,
-                      resultDesc: succResDesc
-                 } 
-           }  
-      }).save()
-        .then((data) => {
+            method: 'mpesa',
+            userId: 'failed',
+            status: 'complete',
+            mpesaObject: {
+                reqID: merchantID,
+                status: {
+                    resultCode: resCode,
+                    resultDesc: resDesc
+                }
+            }
 
-            console.log('--SUCCESSFUL TRANSACTION SAVED')
-        
+        }).save().then((data) => {
+
+            console.log('--INCOMPLETE TRANX SAVED--')
+        }).catch((err) => {
+            console.log('--FAILED TO SAVE TRANX--')
         })
-        .catch( error  => console.log('--ERROR SAVING TRANX SUCC')) 
 
-      
-    return res.json({ message: 'Response OK'})
-   }
-        
+        return res.json({ message: 'Response OK' })
+
+    } else {
+
+        const merchantID = stkCallback.MerchantRequestID
+        const mpesaAmount = metaData.Item[0].Value
+        const phoneNumber = metaData.Item[4].Value
+        const succResCode = stkCallback.ResultCode
+        const succResDesc = stkCallback.ResultDesc
+
+        // todo: SAVE SUCCESSFUL TRANSACTION
+
+        const newSuccTranx = new Transactions({
+            tranx_type: 'Deposit',
+            method: 'mpesa',
+            userId: 'success',
+            amount: mpesaAmount,
+            status: 'pending',
+            mpesaObject: {
+                mpesaNumber: phoneNumber,
+                reqID: merchantID,
+                status: {
+                    resultCode: succResCode,
+                    resultDesc: succResDesc
+                }
+            }
+        }).save()
+            .then((data) => {
+
+                console.log('--SUCCESSFUL TRANSACTION SAVED')
+
+            })
+            .catch(error => console.log('--ERROR SAVING TRANX SUCC'))
+
+
+        return res.json({ message: 'Response OK' })
+    }
+
 })
 
 
@@ -202,10 +202,10 @@ router.get('/', (req, res) => {
 
 
 router.patch('/confirm', async (req, res) => {
-    
-    const userId = req.user._id 
+
+    const userId = req.user._id
     const { reqid } = req.body
-    const transaction = await Transactions.findOne({ "mpesaObject.reqID": reqid  })
+    const transaction = await Transactions.findOne({ "mpesaObject.reqID": reqid })
 
     if (!transaction) {
 
@@ -216,47 +216,47 @@ router.patch('/confirm', async (req, res) => {
 
         try {
 
-            
-          const userObject = await User.findOne({ _id: userId })
-          const mpesaNumber = userObject.mpesa 
-          const currentBalance = userObject.totalBalance
-          const tranxAmount = transaction.amount
-          const tranxPhone = transaction.mpesaObject.mpesaNumber
-          const newBalance = currentBalance + tranxAmount
-          const resDesc = transaction.mpesaObject.status.resultDesc
-          const tranxId = transaction._id
 
-        if (tranxAmount === 0) {
+            const userObject = await User.findOne({ _id: userId })
+            const mpesaNumber = userObject.mpesa
+            const currentBalance = userObject.totalBalance
+            const tranxAmount = transaction.amount
+            const tranxPhone = transaction.mpesaObject.mpesaNumber
+            const newBalance = currentBalance + tranxAmount
+            const resDesc = transaction.mpesaObject.status.resultDesc
+            const tranxId = transaction._id
 
-           req.flash('error', resDesc)
-           res.redirect('/deposit')
-         
-        } else {
+            if (tranxAmount === 0) {
 
-            if (mpesaNumber === 254) {
-               
-               const updatedUser = await User.findByIdAndUpdate({ _id: userId }, { totalBalance: newBalance, mpesa: tranxPhone  })
-               const updatedTranx = await Transactions.findByIdAndUpdate({ _id: tranxId }, { status: 'complete', userId: userId } )
+                req.flash('error', resDesc)
+                res.redirect('/deposit')
 
-               req.flash('error', 'Deposit Complete')
-               res.redirect('/main')
-        
-        } else {
+            } else {
 
-            const updatedUser = await User.findByIdAndUpdate({ _id: userId }, { totalBalance: newBalance })
-            const updatedTranx = await Transactions.findByIdAndUpdate({ _id: tranxId }, { status: 'complete', userId: userId })
+                if (mpesaNumber === 254) {
 
-            req.flash('error', 'Deposit Complete')
-            res.redirect('/main')
-        }
+                    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, { totalBalance: newBalance, mpesa: tranxPhone })
+                    const updatedTranx = await Transactions.findByIdAndUpdate({ _id: tranxId }, { status: 'complete', userId: userId })
 
-    }    
-            
+                    req.flash('error', 'Deposit Complete')
+                    res.redirect('/main')
+
+                } else {
+
+                    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, { totalBalance: newBalance })
+                    const updatedTranx = await Transactions.findByIdAndUpdate({ _id: tranxId }, { status: 'complete', userId: userId })
+
+                    req.flash('error', 'Deposit Complete')
+                    res.redirect('/main')
+                }
+
+            }
+
         } catch (error) {
 
             req.flash('error', 'Transaction Not recorded..CONTACT 0701280373')
             res.redirect('/main')
-            
+
         }
 
     }
