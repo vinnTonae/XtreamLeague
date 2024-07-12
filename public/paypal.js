002c2f5560b572cb961fd652122ab11debf45d90
vinnTonae <alvinkarms5511@gmail.com>
2024-07-13 00:59:10 +0300

    final Project changes

--- public/paypal.js
+++ public/paypal.js
@@ -7,26 +7,36 @@ const lipaBtn = document.getElementById('lipa-mpesa')
 const mpesaCloseBtn = document.querySelector('.close-mpesa')
 const mpesaInputAmount = document.getElementById('mpesa-amount')
 const displayAmount = document.getElementById('displayAmount')
+const submitBtn = document.querySelector('.submit')
+const mpesaForm = document.querySelector('.formbox-mpesa')
 
- 
+// TODO: SUBMIT BTN DISAPPEARS AFTER SUBMIT
+
+mpesaForm.addEventListener('submit', () => {
+    submitBtn.disabled = true
+    submitBtn.style.backgroundColor = 'gray'
+})
+
+
+
 
 function display() {
     paypalBox.style.display = 'block'
     mainBox.style.display = 'none'
 
-  }
+}
 
-  
+
 closeBtn.addEventListener('click', () => {
     paypalBox.style.display = 'none'
-    mainBox.style.display = 'block' 
+    mainBox.style.display = 'block'
 
 })
 
 lipaBtn.addEventListener('click', () => {
     mpesaContainer.style.display = 'flex'
     paypalBox.style.display = 'none'
-     
+
 })
 
 mpesaCloseBtn.addEventListener('click', () => {
@@ -37,44 +47,44 @@ mpesaCloseBtn.addEventListener('click', () => {
 
 
 function purchase(token) {
-    if (token == 100 ) {
+    if (token == 100) {
         inputAmount.setAttribute('value', 1.08)
         mpesaInputAmount.setAttribute('value', 100)
         displayAmount.textContent = '100'
         console.log(mpesaInputAmount.value)
         display()
-    } else if (token == 200 ) {
+    } else if (token == 200) {
         inputAmount.setAttribute('value', 1.85)
         mpesaInputAmount.setAttribute('value', 200)
         displayAmount.textContent = '200'
         console.log(mpesaInputAmount.value)
         display()
-    } else if (token == 500 ) {
+    } else if (token == 500) {
         inputAmount.setAttribute('value', 4.25)
         mpesaInputAmount.setAttribute('value', 500)
         displayAmount.textContent = '500'
         console.log(mpesaInputAmount.value)
         display()
-    } else if (token == 1000 ) {
+    } else if (token == 1000) {
         inputAmount.setAttribute('value', 8.15)
         mpesaInputAmount.setAttribute('value', 1000)
         displayAmount.textContent = '1000'
         console.log(mpesaInputAmount.value)
         display()
-    } else if (token == 2000 ) {
+    } else if (token == 2000) {
         inputAmount.setAttribute('value', 16.45)
         mpesaInputAmount.setAttribute('value', 2000)
         displayAmount.textContent = '2000'
         console.log(mpesaInputAmount.value)
         display()
-    } else if (token == 5000 ) {
+    } else if (token == 5000) {
         inputAmount.setAttribute('value', 39.45)
         mpesaInputAmount.setAttribute('value', 5000)
         displayAmount.textContent = '5000'
         console.log(mpesaInputAmount.value)
         display()
     }
-   
+
 }
 
 
