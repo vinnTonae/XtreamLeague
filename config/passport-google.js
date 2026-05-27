851d18cb77793f828125f220b0630eeb0ddd9504
vinnTonae <136140466+vinnTonae@users.noreply.github.com>
2026-05-27 13:34:19 +0300

    Update Google OAuth callback URL to use MY_DOMAIN

--- config/passport-google.js
+++ config/passport-google.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.LOCALHOST}/auth/google/redirect`
+    callbackURL: `${process.env.MY_DOMAIN}/auth/google/redirect`
 
 }, (accessToken, refreshToken, profile, done) => {
     //Check if User exists in the site
